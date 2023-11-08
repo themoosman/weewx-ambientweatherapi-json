e38c9754585e094d6ba7b8dde46472475f87a4ed
Karl Moos <kmoos@redhat.com>
2023-11-07 19:48:34 -0600

    Fix formatting issues

--- install.py
+++ install.py
@@ -10,14 +10,10 @@ def loader():
 class AmbientWeatherApiInstaller(ExtensionInstaller):
     def __init__(self):
         super(AmbientWeatherApiInstaller, self).__init__(
-            version="0.7",
+            version="0.8",
             name='ambientweatherapi',
             description='WeeWx AmbientWeather API Driver.',
             author="Karl Moos",
             restful_services='user.ambientweatherapi.AmbientWeatherAPI',
-            config={
-                'StdRESTful': {
-                    'Windy': {
-                        'api_key': 'replace_me'}}},
             files=[('bin/user', ['bin/user/ambientweatherapi.py'])]
         )
