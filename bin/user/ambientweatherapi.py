d1e0852459efe7109cb298959623b52558dd3254
Karl Moos <kmoos@redhat.com>
2024-04-11 20:46:29 -0500

    Thu Apr 11 08:46:29 PM CDT 2024

--- bin/user/ambientweatherapi.py
+++ bin/user/ambientweatherapi.py
@@ -106,10 +106,9 @@ class AmbientWeatherAPI(weewx.drivers.AbstractDevice):
 
     def print_dict(self, data_dict):
         """Prints a dict."""
-        if logging.INFO >= logging.root.level:
-            logging.debug("calling: print_dict")
-            for key in data_dict:
-                logging.debug(key + " = " + str(data_dict[key]))
+        logging.info("calling: print_dict")
+        for key in data_dict:
+            logging.into(key + " = " + str(data_dict[key]))
 
     def get_value(self, data_dict, key):
         """Gets the value from a dict, returns None if the key does not exist."""
