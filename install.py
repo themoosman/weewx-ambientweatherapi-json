4f58c8923659cccea1c966a662f09638fc9d8357
Karl Moos <themoosman@users.noreply.github.com>
2024-06-16 21:31:58 -0500

    Update Installer (#33)

--- install.py
+++ install.py
@@ -10,11 +10,10 @@ def loader():
 class AmbientWeatherApiInstaller(ExtensionInstaller):
     def __init__(self):
         super(AmbientWeatherApiInstaller, self).__init__(
-            version="0.0.11",
+            version="0.0.12",
             name='ambientweatherapi',
             description='WeeWx AmbientWeather API Driver.',
             author="Karl Moos",
-            restful_services='user.ambientweatherapi.AmbientWeatherAPI',
             config={
                 'ambientweatherapi': {
                     'driver': 'user.ambientweatherapi',
